c5a8dc5c43915838c5f7250adfb7d406ce6ac433
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-02-07 13:27:50 -0300

    Initial commit

---
+++ tech_news/analyzer/search_engine.py
@@ -0,0 +1,13 @@
+# Requisito 7
+def search_by_title(title):
+    """Seu código deve vir aqui"""
+
+
+# Requisito 8
+def search_by_date(date):
+    """Seu código deve vir aqui"""
+
+
+# Requisito 9
+def search_by_category(category):
+    """Seu código deve vir aqui"""
