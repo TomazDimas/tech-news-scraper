6746c3f3728cb85a8db1a6f776d7321a7ad1c461
TomazDimas <tomazdimas2001@gmail.com>
2023-02-16 16:25:17 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,10 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_url = selector.css("a.next.page-numbers::attr(href)").get()
+    print(next_url)
+    return next_url
 
 
 # Requisito 4
