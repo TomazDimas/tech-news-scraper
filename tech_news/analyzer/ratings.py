7c8ffe20a3d91ce1c7695a2aeb4a72cf71837651
TomazDimas <tomazdimas2001@gmail.com>
2023-02-27 17:56:34 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    news_popularity = {}
+    for new in all_news:
+        if new["category"] not in news_popularity:
+            news_popularity[new["category"]] = 1
+        else:
+            news_popularity[new["category"]] += 1
+
+    sorted_popularity = sorted(
+        sorted(news_popularity), key=news_popularity.get, reverse=True
+    )
+    return sorted_popularity
+
+
+top_5_categories()
