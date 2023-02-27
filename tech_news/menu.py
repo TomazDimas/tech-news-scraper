8d5148803ddbf9ab4bddba81e2a76576197f7ea6
TomazDimas <tomazdimas2001@gmail.com>
2023-02-27 18:20:20 -0300

    Requisito 12 Incompleto

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,11 @@
 import sys
-# from tech_news.scraper import get_tech_news
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_categories
 
 
 # Requisitos 11 e 12
@@ -14,20 +20,30 @@ def analyzer_menu():
  5 - Sair.
  """
     )
-    if first_option == 0:
-        # news_amount = input
-        print(("Digite quantas notícias serão buscadas:"))
-    elif first_option == 1:
-        # title = input("Digite o título:")
-        print(("Digite o título:"))
-    elif first_option == 2:
-        # title = input("Digite a data no formato aaaa-mm-dd:")
-        print(("Digite a data no formato aaaa-mm-dd:"))
-    elif first_option == 3:
-        # category = input("Digite a categoria:")
-        print(("Digite a data no formato aaaa-mm-dd:"))
+    if int(first_option) == 0:
+        news_amount = input("Digite quantas notícias serão buscadas:")
+        get_tech_news(int(news_amount))
+        # print(("Digite quantas notícias serão buscadas:"))
+    elif int(first_option) == 1:
+        title = input("Digite o título:")
+        response = search_by_title(title)
+        print(response)
+        # print(("Digite o título:"))
+    elif int(first_option) == 2:
+        date = input("Digite a data no formato aaaa-mm-dd:")
+        response = search_by_date(date)
+        print(response)
+        # print(("Digite a data no formato aaaa-mm-dd:"))
+    elif int(first_option) == 3:
+        category = input("Digite a categoria:")
+        response = search_by_category(category)
+        print(response)
+        # print(("Digite a data no formato aaaa-mm-dd:"))
+    elif int(first_option) == 4:
+        response = top_5_categories()
+        print(response)
+    elif int(first_option) == 5:
+        print("Encerrando script")
+        return
     else:
         sys.stderr.write("Opção inválida")
-
-
-# analyzer_menu()
